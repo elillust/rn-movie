992c42fa0e9717a5c3b6db8b0294df8f110db951
까망베르냥 <cute.lovely.cat@kakao.com>
2022-05-03 14:15:15 +0900

    study

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
-import { Text, Image } from "react-native";
+import * as React from 'react';
+import { View, Text, Image } from "react-native";
 import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { Asset, useAssets } from 'expo-asset';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator(); 
+
+function HomeScreen() {
+  return (
+    <View style={{ flex:1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingScreen() {
+  return (
+    <View style={{ flex:1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Setting!</Text>
+    </View>
+  );
+}
 
 export default function App() {  
   const [assets] = useAssets([require("./profil-01.jpg")]);
@@ -14,7 +34,12 @@ export default function App() {
       <AppLoading />
     );
   }
-  return (
-    <Text>We are dddcccc done loading Test</Text>
+  return (    
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Setting" component={SettingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,3 +1,15 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
 
-const Tab = createBottomTabNavigator(); 
+const Nav = createBottomTabNavigator(); 
+const Tabs = () => (
+  <Nav.Navigator>
+    <Nav.Screen name="Movies" component={Movies} />
+    <Nav.Screen name="Tv" component={Tv} />
+    <Nav.Screen name="Search" component={Search} />
+  </Nav.Navigator>
+);
+
+export default Tabs;
